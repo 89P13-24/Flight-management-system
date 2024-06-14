a47ea6dfdf168cde75b6794e034315ebba63304d
ishan <g.ishan@iitg.ac.in>
2024-06-14 18:25:42 +0530

    Completed Google Authentication

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqwpOqR5kATS8U6oLlrSHBJByb6qjKPpI",
+  authDomain: "airline-db-47aee.firebaseapp.com",
+  projectId: "airline-db-47aee",
+  storageBucket: "airline-db-47aee.appspot.com",
+  messagingSenderId: "41064046644",
+  appId: "1:41064046644:web:bd1c0f6866426b71bf2a61"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
