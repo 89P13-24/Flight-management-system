cb2ae58cb6eaf3093b8c805ee3c3ec137b8c6b41
ishan <g.ishan@iitg.ac.in>
2024-06-14 19:23:33 +0530

    Completed profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {FaUser} from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 export default function Header() {
+  const {currentUser} = useSelector((state)=> state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className="bg-slate-200 p-3 flex justify-between mx-auto max-w-6xl">
@@ -21,8 +23,10 @@ export default function Header() {
             <Link to='/sign-in'>
             <li><FaUser className='text-slate-700'/></li>
             </Link>
-            <Link to='/sign-in'>
-            <li className='text-slate-700 hover:underline'>Sign-in</li>
+            <Link to='/profile'>
+            {currentUser ? <img src= {currentUser.avatar}  alt = "profile" className='rounded-full h-7 w-7 object-cover'></img> 
+            :<li className='text-slate-700 hover:underline'>Sign-in</li>
+            }
             </Link>
         </ul>
         </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 export default function Profile() {
+  const {currentUser} = useSelector((state)=> state.user);
   return (
-    <div>Profile</div>
+    <div className="max-w-lg mx-auto p-3">
+      <h1 className='text-3xl font-semibold my-7 text-center'>Profile</h1>
+      <form className="flex flex-col gap-4">
+      <img src={currentUser.avatar} alt="profile" className="self-center rounded-full h-24 w-24 object-cover mt-2 cursor-pointer"/>
+      <input type="text" placeholder="username" id = "username" className="p-3 rounded-lg border"/>
+      <input type="email" placeholder="email" id = "email" className="p-3 rounded-lg border"/>
+      <input type="text" placeholder="password" id = "password" className="p-3 rounded-lg border"/>
+      <button className="bg-slate-700 uppercase p-3 rounded-lg text-white hover:opacity-95 disabled:opacity-80">update</button>
+      
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+      
+    </div>
   )
 }
