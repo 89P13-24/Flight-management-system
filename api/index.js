4069d8c6223d7b077a5f40e123fd811ea024c181
ishan <g.ishan@iitg.ac.in>
2024-06-14 22:37:05 +0530

    Delete Account done

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.listen(3000,()=>{
     console.log("Server listening to port 3000!!!");
 });
 
-app.use('/test',userRouter);
+app.use('/api/user', userRouter);
 app.use('/api/auth',authRouter);
 
 app.use((err,req,res,next)=>{
